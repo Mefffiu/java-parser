be3f0df75a48a53f3b44d61400b26de80cc26a5c
Damian Falana <falanadamian@gmail.com>
2019-04-01 22:22:08 +0200

    Reduced number of recognizable structures

--- src/main/java/DataCollector.java
+++ src/main/java/DataCollector.java
@@ -16,60 +16,30 @@ public class DataCollector {
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     private static final Map<Class, String> structures = new HashMap<>();
+
     static {
         structures.put(Java8Parser.CompilationUnitContext.class, "file_content"); // highest order structure
 
-        structures.put(Java8Parser.LambdaBodyContext.class, "lambda_body");
-        structures.put(Java8Parser.LambdaParametersContext.class, "lambda_parameters");
-        structures.put(Java8Parser.LambdaExpressionContext.class, "lambda_expression");
-        structures.put(Java8Parser.ArrayCreationExpressionContext.class, "array_creation_expression");
         structures.put(Java8Parser.MethodInvocationContext.class, "method_invocation");
-        structures.put(Java8Parser.ClassInstanceCreationExpressionContext.class, "class_instance_creation_expression");
         structures.put(Java8Parser.TryWithResourcesStatementContext.class, "try_with_resources_statement");
         structures.put(Java8Parser.Finally_Context.class, "finally_");
         structures.put(Java8Parser.CatchClauseContext.class, "catch_clause");
         structures.put(Java8Parser.TryStatementContext.class, "try_statement");
-        structures.put(Java8Parser.ThrowStatementContext.class, "throw_statement");
         structures.put(Java8Parser.ReturnStatementContext.class, "return_statement");
-        structures.put(Java8Parser.EnhancedForStatementContext.class, "enhanced_for_statement");
-        structures.put(Java8Parser.BasicForStatementContext.class, "basic_for_statement");
         structures.put(Java8Parser.ForStatementContext.class, "for_statement");
         structures.put(Java8Parser.DoStatementContext.class, "do_statement");
         structures.put(Java8Parser.WhileStatementContext.class, "while_statement");
-        structures.put(Java8Parser.EnumConstantNameContext.class, "enum_constant_name");
-        structures.put(Java8Parser.SwitchLabelContext.class, "switch_label");
-        structures.put(Java8Parser.SwitchLabelsContext.class, "switch_labels");
-        structures.put(Java8Parser.SwitchBlockStatementGroupContext.class, "switch_block_statement_group");
-        structures.put(Java8Parser.SwitchBlockContext.class, "switch_block");
         structures.put(Java8Parser.SwitchStatementContext.class, "switch_statement");
         structures.put(Java8Parser.IfThenElseStatementContext.class, "if_then_else_statement");
         structures.put(Java8Parser.IfThenStatementContext.class, "if_then_statement");
-        structures.put(Java8Parser.LocalVariableDeclarationContext.class, "local_variable_declaration");
-        structures.put(Java8Parser.LocalVariableDeclarationStatementContext.class, "local_variable_declaration_statement");
         structures.put(Java8Parser.ConstantDeclarationContext.class, "constant_declaration");
-        structures.put(Java8Parser.InterfaceBodyContext.class, "interface_body");
-        structures.put(Java8Parser.ExtendsInterfacesContext.class, "extends_interfaces");
         structures.put(Java8Parser.InterfaceDeclarationContext.class, "interface_declaration");
-        structures.put(Java8Parser.EnumBodyDeclarationsContext.class, "enum_body_declarations");
-        structures.put(Java8Parser.EnumConstantContext.class, "enum_constant");
-        structures.put(Java8Parser.EnumBodyContext.class, "enum_body");
         structures.put(Java8Parser.EnumDeclarationContext.class, "enum_declaration");
-        structures.put(Java8Parser.ExplicitConstructorInvocationContext.class, "explicit_constructor_invocation");
-        structures.put(Java8Parser.ConstructorBodyContext.class, "constructor_body");
         structures.put(Java8Parser.ConstructorDeclarationContext.class, "constructor_declaration");
-        structures.put(Java8Parser.MethodBodyContext.class, "method_body");
-        structures.put(Java8Parser.MethodHeaderContext.class, "method_header");
         structures.put(Java8Parser.MethodDeclarationContext.class, "method_declaration");
         structures.put(Java8Parser.ClassMemberDeclarationContext.class, "class_member_declaration");
-        structures.put(Java8Parser.ClassBodyContext.class, "class_body");
-        structures.put(Java8Parser.SuperinterfacesContext.class, "superinterfaces");
-        structures.put(Java8Parser.SuperclassContext.class, "superclass");
         structures.put(Java8Parser.ClassDeclarationContext.class, "class_declaration");
-        structures.put(Java8Parser.SingleStaticImportDeclarationContext.class, "single_static_import_declaration");
-        structures.put(Java8Parser.ImportDeclarationContext.class, "import_declaration");
         structures.put(Java8Parser.PackageDeclarationContext.class, "package_declaration");
-        structures.put(Java8Parser.MethodNameContext.class, "method_name");
-        structures.put(Java8Parser.PackageNameContext.class, "package_name");
     }
 
     private List<DataRow> dataRows;
--- src/main/java/Visitor.java
+++ src/main/java/Visitor.java
@@ -10,39 +10,9 @@ public class Visitor extends Java8BaseVisitor<String> {
     }
 
     @Override
-    public String visitLambdaBody(Java8Parser.LambdaBodyContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitLambdaBody(ctx);
-    }
-
-    @Override
-    public String visitLambdaParameters(Java8Parser.LambdaParametersContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitLambdaParameters(ctx);
-    }
-
-    @Override
-    public String visitLambdaExpression(Java8Parser.LambdaExpressionContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitLambdaExpression(ctx);
-    }
-
-    @Override
-    public String visitArrayCreationExpression(Java8Parser.ArrayCreationExpressionContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitArrayCreationExpression(ctx);
-    }
-
-    @Override
     public String visitMethodInvocation(Java8Parser.MethodInvocationContext ctx) {
         this.dataCollector.collectDataRow(ctx);
         return super.visitMethodInvocation(ctx);
-    }
-
-    @Override
-    public String visitClassInstanceCreationExpression(Java8Parser.ClassInstanceCreationExpressionContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitClassInstanceCreationExpression(ctx);
     }
 
     @Override
@@ -70,27 +40,9 @@ public class Visitor extends Java8BaseVisitor<String> {
     }
 
     @Override
-    public String visitThrowStatement(Java8Parser.ThrowStatementContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitThrowStatement(ctx);
-    }
-
-    @Override
     public String visitReturnStatement(Java8Parser.ReturnStatementContext ctx) {
         this.dataCollector.collectDataRow(ctx);
         return super.visitReturnStatement(ctx);
-    }
-
-    @Override
-    public String visitEnhancedForStatement(Java8Parser.EnhancedForStatementContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitEnhancedForStatement(ctx);
-    }
-
-    @Override
-    public String visitBasicForStatement(Java8Parser.BasicForStatementContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitBasicForStatement(ctx);
     }
 
     @Override
@@ -112,36 +64,6 @@ public class Visitor extends Java8BaseVisitor<String> {
     }
 
     @Override
-    public String visitEnumConstantName(Java8Parser.EnumConstantNameContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitEnumConstantName(ctx);
-    }
-
-    @Override
-    public String visitSwitchLabel(Java8Parser.SwitchLabelContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitSwitchLabel(ctx);
-    }
-
-    @Override
-    public String visitSwitchLabels(Java8Parser.SwitchLabelsContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitSwitchLabels(ctx);
-    }
-
-    @Override
-    public String visitSwitchBlockStatementGroup(Java8Parser.SwitchBlockStatementGroupContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitSwitchBlockStatementGroup(ctx);
-    }
-
-    @Override
-    public String visitSwitchBlock(Java8Parser.SwitchBlockContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitSwitchBlock(ctx);
-    }
-
-    @Override
     public String visitSwitchStatement(Java8Parser.SwitchStatementContext ctx) {
         this.dataCollector.collectDataRow(ctx);
         return super.visitSwitchStatement(ctx);
@@ -160,33 +82,9 @@ public class Visitor extends Java8BaseVisitor<String> {
     }
 
     @Override
-    public String visitLocalVariableDeclaration(Java8Parser.LocalVariableDeclarationContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitLocalVariableDeclaration(ctx);
-    }
-
-    @Override
-    public String visitLocalVariableDeclarationStatement(Java8Parser.LocalVariableDeclarationStatementContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitLocalVariableDeclarationStatement(ctx);
-    }
-
-    @Override
     public String visitConstantDeclaration(Java8Parser.ConstantDeclarationContext ctx) {
         this.dataCollector.collectDataRow(ctx);
         return super.visitConstantDeclaration(ctx);
-    }
-
-    @Override
-    public String visitInterfaceBody(Java8Parser.InterfaceBodyContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitInterfaceBody(ctx);
-    }
-
-    @Override
-    public String visitExtendsInterfaces(Java8Parser.ExtendsInterfacesContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitExtendsInterfaces(ctx);
     }
 
     @Override
@@ -196,57 +94,15 @@ public class Visitor extends Java8BaseVisitor<String> {
     }
 
     @Override
-    public String visitEnumBodyDeclarations(Java8Parser.EnumBodyDeclarationsContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitEnumBodyDeclarations(ctx);
-    }
-
-    @Override
-    public String visitEnumConstant(Java8Parser.EnumConstantContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitEnumConstant(ctx);
-    }
-
-    @Override
-    public String visitEnumBody(Java8Parser.EnumBodyContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitEnumBody(ctx);
-    }
-
-    @Override
     public String visitEnumDeclaration(Java8Parser.EnumDeclarationContext ctx) {
         this.dataCollector.collectDataRow(ctx);
         return super.visitEnumDeclaration(ctx);
     }
 
     @Override
-    public String visitExplicitConstructorInvocation(Java8Parser.ExplicitConstructorInvocationContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitExplicitConstructorInvocation(ctx);
-    }
-
-    @Override
-    public String visitConstructorBody(Java8Parser.ConstructorBodyContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitConstructorBody(ctx);
-    }
-
-    @Override
     public String visitConstructorDeclaration(Java8Parser.ConstructorDeclarationContext ctx) {
         this.dataCollector.collectDataRow(ctx);
         return super.visitConstructorDeclaration(ctx);
-    }
-
-    @Override
-    public String visitMethodBody(Java8Parser.MethodBodyContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitMethodBody(ctx);
-    }
-
-    @Override
-    public String visitMethodHeader(Java8Parser.MethodHeaderContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitMethodHeader(ctx);
     }
 
     @Override
@@ -262,62 +118,14 @@ public class Visitor extends Java8BaseVisitor<String> {
     }
 
     @Override
-    public String visitClassBody(Java8Parser.ClassBodyContext ctx) {
+    public String visitClassDeclaration(Java8Parser.ClassDeclarationContext ctx) {
         this.dataCollector.collectDataRow(ctx);
-        return super.visitClassBody(ctx);
-    }
-
-    @Override
-    public String visitSuperinterfaces(Java8Parser.SuperinterfacesContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitSuperinterfaces(ctx);
-    }
-
-    @Override
-    public String visitSuperclass(Java8Parser.SuperclassContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitSuperclass(ctx);
-    }
-
-    @Override
-    public String visitNormalClassDeclaration(Java8Parser.NormalClassDeclarationContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitNormalClassDeclaration(ctx);
-    }
-
-    @Override
-    public String visitSingleStaticImportDeclaration(Java8Parser.SingleStaticImportDeclarationContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitSingleStaticImportDeclaration(ctx);
-    }
-
-    @Override
-    public String visitSingleTypeImportDeclaration(Java8Parser.SingleTypeImportDeclarationContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitSingleTypeImportDeclaration(ctx);
+        return super.visitClassDeclaration(ctx);
     }
 
     @Override
     public String visitPackageDeclaration(Java8Parser.PackageDeclarationContext ctx) {
         this.dataCollector.collectDataRow(ctx);
         return super.visitPackageDeclaration(ctx);
-    }
-
-    @Override
-    public String visitCompilationUnit(Java8Parser.CompilationUnitContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitCompilationUnit(ctx);
-    }
-
-    @Override
-    public String visitMethodName(Java8Parser.MethodNameContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitMethodName(ctx);
-    }
-
-    @Override
-    public String visitPackageName(Java8Parser.PackageNameContext ctx) {
-        this.dataCollector.collectDataRow(ctx);
-        return super.visitPackageName(ctx);
     }
 }
